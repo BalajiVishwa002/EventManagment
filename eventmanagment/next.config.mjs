c8fbc44735abe15f26fbeee281be8d9720b78cbb
BalajiVishwa002 <balajiviswanathan98@gmail.com>
2025-07-05 17:21:39 +0530

    base urtl, changed

--- eventmanagment/next.config.mjs
+++ eventmanagment/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     env:{
         NEXTAUTH_SECRET:"NEXTAUTH_SECRET=dEJ+VnVJcGxWZnU0MzM4L2hxZHZKZUNLbC0xZFV0bjk=",
-        BASE_URL:"http://192.168.34.182:8000/"
+        BASE_URL:"http://em.appblocky.com/"
     }
 };
 
